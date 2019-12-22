3f8e1045d9861c6dbbafdfb220d9bfa7122c0c1e
Yehor Sedinin <yehor.sedinin@competera.net>
2019-12-22 20:23:23 +0200

    Initializing remote db

--- source/dao/data.py
+++ source/dao/data.py
@@ -6,4 +6,4 @@ host = '127.0.0.1'
 port = '5432'
 database = 'postgres'
 DATABASE_URI = os.getenv("DATABASE_URL",
-                         'postgres+psycopg2://postgres:{}@{}:{}/{}'.format(password, host, port, database))
+                         ' postgres://xyikxvguklthze:74e3a7a712173572b976791f1f1e607cc29838d23e1572c37e3a2881f499729e@ec2-174-129-24-148.compute-1.amazonaws.com:5432/d1gnq35nqlip83'.format(password, host, port, database))
